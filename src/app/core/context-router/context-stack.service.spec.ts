defb5a21fb7939380b816bbf1a80cb167a27a0a7
saiki <saiki@dcr.co.jp>
2018-06-18 11:09:17 +0900

    create red test

--- src/app/core/context-router/context-stack.service.spec.ts
+++ src/app/core/context-router/context-stack.service.spec.ts
@@ -1,3 +1,4 @@
+import { PageContext } from './page-context';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { ContextStackService } from './context-stack.service';
@@ -5,11 +6,27 @@ import { ContextStackService } from './context-stack.service';
 describe('ContextStackService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ContextStackService]
+      providers: [ContextStackService],
     });
   });
 
-  it('should be created', inject([ContextStackService], (service: ContextStackService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('stack and takeoff', inject(
+    [ContextStackService],
+    (service: ContextStackService) => {
+      const newpage = new PageContext();
+      newpage.contextId = '/id0001/:id';
+      newpage.data = {
+        list: [
+          { name: 'username', value: 'taro' },
+          { name: 'age', value: 123 },
+        ],
+      };
+
+      service.stack(newpage);
+
+      const getpage = service.takeOff('/id0001/:id');
+      expect(getpage).not.toBeNull();
+      expect(getpage.contextId).toEqual('/id0001/:id');
+    },
+  ));
 });
